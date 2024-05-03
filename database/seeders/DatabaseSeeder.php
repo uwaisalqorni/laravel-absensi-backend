c626d969b0b94c5b050082b3d16fc123b23ab564
Muhammad Musa <oktaimtiziliffa@gmail.com>
2024-05-03 18:05:19 +0700

    add dashboard company & API

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,17 @@ class DatabaseSeeder extends Seeder
             'email' => 'afandi@fic16.com',
             'password' => Hash::make('12345678'),
         ]);
+
+        // data dummy for company
+        \App\Models\Company::create([
+            'name' => 'Rumah Sakit Islam Gondanglegi',
+            'email' => 'rsigondanglegi@ymail.com',
+            'address' => 'Jl. Hayam Wuruk No.66 Gondanglegi',
+            'latitude' => '-8,1702801',
+            'longitude' => '112,6401149',
+            'radius_km' => '13.5',
+            'time_in' => '08:00',
+            'time_out' => '15:00',
+        ]);
     }
 }
